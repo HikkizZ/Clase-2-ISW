0963123609c8bb91b488c31397fc9d91b07776bf
HikkizZ <fmiranda.reb@gmail.com>
2024-09-05 01:02:28 -0400

    AssignmentAndComparisonOperators.js
    Tarea 2 - Ejercicio 3

---
+++ AssignmentAndComparisonOperators.js
@@ -0,0 +1,21 @@
+const numero1 = 15;
+const numero2 = 20;
+const numero3 = '25';
+
+// Muestre por consola si el número1 es mayor o igual que número2
+console.log(numero1 >= numero2);
+
+// Muestre por consola si el número1 es menor o igual que número2
+console.log(numero1 <= numero2);
+
+// Muestre por consola si el número1 es menor que numero3
+console.log(numero1 < numero3);
+
+// Muestre por consola si el número3 es manor que numero2
+console.log(numero3 < numero2);
+
+// Muestre por consola si el número3 es estriamente diferente que número1
+console.log(numero3 !== numero1);
+
+// Muestre por consola si el número1 es estriamente igual que número2
+console.log(numero1 === numero2);
