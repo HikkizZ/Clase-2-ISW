f723bac2a498c3ed8140400a79c796a3769f3c54
HikkizZ <fmiranda.reb@gmail.com>
2024-09-05 00:51:08 -0400

    ADD VariablesAndDataTypes.js
    Tarea 2 - Ejercicio 1

---
+++ VariablesAndDataTypes.js
@@ -0,0 +1,24 @@
+/*
+1. Declara una variable llamada `nombre` y asígnale un valor de tipo `string`.
+2. Declara una variable llamada edad y asígnale un valor de tipo `number`.
+3. Declara una variable llamada `puedoConducir` y asígnale un valor de tipo `boolean` .
+4. Para cada variable se debe imprimir por consola el resultado de cada variable declarada.
+5. Para cada variable se debe imprimir el tipo de dato que es por consola.
+*/
+
+// Declarar variable tipo string
+const nombre = 'Juan';
+console.log(nombre); // Imprimir variable por consola
+
+// Declarar variable tipo number
+const edad = 18;
+console.log(edad); // Imprimir variable por consola
+
+// Declarar variable tipo boolean
+const puedoConducir = true;
+console.log(puedoConducir); // Imprimir variable por consola
+
+// Imprimir el tipo de dato de cada variable
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof puedoConducir);
