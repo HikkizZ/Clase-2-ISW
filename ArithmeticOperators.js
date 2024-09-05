fd25d6c39d5766205dc6a70b19ca0e38adf2d5e2
HikkizZ <fmiranda.reb@gmail.com>
2024-09-05 00:54:42 -0400

    ADD ArithmeticOperators.js
    Tarea 2 - Ejercicio 2

---
+++ ArithmeticOperators.js
@@ -0,0 +1,22 @@
+const numero1 = 20;
+const numero2 = 40;
+
+// Addition
+const suma = numero1 + numero2;
+console.log(suma); // 60
+
+// Subtraction
+const resta = numero1 - numero2;
+console.log(resta); // -20
+
+// Multiplication
+const multiplicacion = numero1 * numero2;
+console.log(multiplicacion); // 800
+
+// Division
+const division = numero1 / numero2;
+console.log(division); // 0.5
+
+// Modulus
+const modulo = numero1 % numero2;
+console.log(modulo); // 20
