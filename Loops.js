7c8d1ca310b581877c4965df242a638f119e59b1
HikkizZ <fmiranda.reb@gmail.com>
2024-09-09 22:08:27 -0300

    MOD Loops.js
    Error de tipeo en últimos dos ejercicios

--- Loops.js
+++ Loops.js
@@ -38,14 +38,14 @@ for (let i = 1; i <= 10; i++) {
 
 // Imprimir en consola los números del 1 al 10 pero solo los múltiplos de 3 y 5.
 for (let i = 1; i <= 10; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
         console.log(i);
     }
 }
 
 // Imprimir en consola los números del 1 al 10 pero solo los múltiplos de 3 o 5.
 for (let i = 1; i <= 10; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 3 === 0 || i % 5 === 0) {
         console.log(i);
     }
 }
